e066dbdfe690478dd8fa94105eea7b4a1c9f4d58
gitferry <greferry@gmail.com>
2022-03-14 19:08:13 +0800

    update tr

--- stratus-plot/batchsize/batch-size.py
+++ stratus-plot/batchsize/batch-size.py
@@ -12,7 +12,7 @@ plt.rc('ytick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
 
 bsize = [
-    ('b16K-p128',[
+    ('n128-b32K',[
         # (4,703),
         (58,400),
         (62,300),
@@ -23,8 +23,8 @@ bsize = [
         (77,650),
         (78,900),
         (79,1328),
-    ], '-o', 'coral'),
-    ('b64K-p128',[
+    ], '-o', 'steelblue'),
+    ('n128-b64K',[
         # (12,900),
         # (20,580),
         # (28,447),
@@ -36,8 +36,8 @@ bsize = [
         (130,590),
         (131,779),
         (130,1162),
-    ], '-^', 'coral'),
-    ('b128K-p128',[
+    ], '-^', 'steelblue'),
+    ('n128-128K',[
         # (28,800),
         # (36,640),
         # (44,540),
@@ -49,37 +49,38 @@ bsize = [
         (134,610),
         (135,812),
         (135,1302),
-    ], '-*', 'coral'),
-    ('b16K-p512',[
-        (54,380),
-        (58,250),
-        (62,285),
-        (65,634),
+    ], '-*', 'steelblue'),
+    ('n256-b128K',[
+        (34,880),
+        # (58,250),
+        (42,635),
+        (55,684),
         (65,746),
         (64,1582),
-    ], '--p', 'steelblue'),
-    ('b64K-p512',[
+    ], '--p', 'coral'),
+    ('n256-b256K',[ #125.3, 965
         # (8,653),
-        (66,478),
-        (74,395),
-        (82,375),
-        (90,420),
-        (96,600),
-        (95,1032),
-    ], '--v', 'steelblue'),
-    ('b128K-p512',[
+        (79,1242),
+        (95,965),
+        (103,1031),
+        (114,1234),
+        (125.4,2353),
+        (125.3,2890),
+    ], '--v', 'coral'),
+    ('n256-b512K',[ #125.3, 965
         # (12,843),
         # (20,540),
-        (78,623),
-        (86,568),
+        # (78,1654),
+        (86,1651),
         # (44,320),
-        (102,632),
-        (101,1072),
-        (102,1323)
-    ], '--d', 'steelblue')]
+        (102,1363),
+        (122,1532),
+        (131,2365),
+        (130,3102),
+    ], '--d', 'coral')]
 
 def do_plot():
-    f = plt.figure(1, figsize=(7.5,4), constrained_layout=True)
+    f = plt.figure(1, figsize=(7,3), constrained_layout=True)
     plt.clf()
     ax = f.add_subplot(1, 1, 1)
     for name, entries, style, color in bsize:
@@ -89,9 +90,9 @@ def do_plot():
             throughput.append(t)
             latency.append(l)
         ax.plot(throughput, latency, style, color=color, label='%s' % name, markersize=8, alpha=0.8)
-    plt.legend(fancybox=True,frameon=True,framealpha=0.3,mode={"expand", None},ncol=2, loc='upper right')
+    plt.legend(fancybox=True,frameon=True,framealpha=0.3,mode={"expand", None},ncol=2, loc='upper left')
     plt.grid(linestyle='--', alpha=0.5)
-    plt.ylim([0,2000])
+    plt.ylim([0,3500])
     plt.ylabel('Latency (ms)')
     plt.xlabel('Throughput (KTx/s)')
     # plt.tight_layout()
